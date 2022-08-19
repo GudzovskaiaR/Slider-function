af57528b314abde275c7b961a25c3cf0235b5b47
GudzovskaiaR <100199861+GudzovskaiaR@users.noreply.github.com>
2022-08-19 14:17:50 +0300

    finished creating the slider

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,38 @@
-function Slider(slider){
-  console.log(slider)
-    if (!slider) {
-        throw new Error("Gallery element should be passed!");
-      }
-       
-      let currentContent = slider.querySelector(".slide");
-      const slideContainer = slider.querySelector(".slider-wrapper")
-      const prevButton =slider.querySelector(".btn-prev");
-      const nextButton = slider.querySelector(".btn-next");
-      
+function Slider(slider) {
+  console.log(slider);
+  if (!slider) {
+    throw new Error("Gallery element should be passed!");
+  }
 
-      function showImage(content) {
-      
-        currentContent = content;
-        if (slider.querySelector('img')) {
-          slider.querySelector('img').src = content.querySelector('img').src;
-        } else {
-          slider.querySelector("h2").textContent = content.querySelector("h2").textContent ; 
-        slider.querySelector("p").textContent = content.querySelector("p").textContent ;   
-        }
-        
-           
-      }
+  let currentContent = slider.querySelector(".slide");
+  const slideContainer = slider.querySelector(".slider-wrapper");
+  const prevButton = slider.querySelector(".btn-prev");
+  const nextButton = slider.querySelector(".btn-next");
 
-      function showNextImage(){
-     
-        showImage(currentContent.nextElementSibling || slideContainer.firstElementChild)
-        }
-        function showPrevImage(){
-            
-            showImage(currentContent.previousElementSibling || slideContainer.lastElementChild)
-        }
-        nextButton.addEventListener('click', showNextImage); 
-        prevButton.addEventListener('click', showPrevImage); 
+  function showImage(content) {
+    currentContent = content;
+    if (slider.querySelector("img")) {
+      slider.querySelector("img").src = content.querySelector("img").src;
+    } else {
+      slider.querySelector("h2").textContent =
+        content.querySelector("h2").textContent;
+      slider.querySelector("p").textContent =
+        content.querySelector("p").textContent;
+    }
+  }
+
+  function showNextImage() {
+    showImage(
+      currentContent.nextElementSibling || slideContainer.firstElementChild
+    );
+  }
+  function showPrevImage() {
+    showImage(
+      currentContent.previousElementSibling || slideContainer.lastElementChild
+    );
+  }
+  nextButton.addEventListener("click", showNextImage);
+  prevButton.addEventListener("click", showPrevImage);
 }
 const slider1 = Slider(document.querySelector(".slider1"));
 const slider2 = Slider(document.querySelector(".slider2"));
